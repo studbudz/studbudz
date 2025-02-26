737086ad773e75fd32585fd888b9041c9f31ff25
AlanMet <106497267+AlanMet@users.noreply.github.com>
2025-02-26 15:39:24 +0000

    Add basic WebSocket signaling server implementation

--- server/server.dart
+++ server/server.dart
@@ -1,1 +1,27 @@
+import 'dart:io';
 
+//basic server code
+void main() async {
+  HttpServer server = await HttpServer.bind('0.0.0.0', 8080);
+  print('WebSocket signaling server running on ws://localhost:8080');
+
+  // Handles incoming connections
+  await for (HttpRequest request in server) {
+    if (WebSocketTransformer.isUpgradeRequest(request)) {
+      WebSocket socket = await WebSocketTransformer.upgrade(request);
+      handleConnection(socket);
+    } else {
+      request.response.statusCode = HttpStatus.forbidden;
+      await request.response.close();
+    }
+  }
+}
+
+void handleConnection(WebSocket socket) {
+  socket.listen((message) {
+    print('Message received: $message');
+    socket.add(message);
+  }, onDone: () {
+    print('Client disconnected');
+  });
+}
